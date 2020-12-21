0577128e98fccbc58a523d936d647c45200ed5af
Feinzer <danhernan23i@gmail.com>
2020-12-21 19:36:54 -0400

    Add page transition

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,34 @@ export default Vue.extend({
 <template>
   <div
     id="app"
-    class="flex flex-col min-h-screen bg-light-background dark:bg-dark-background dark:text-white"
+    class="flex flex-col h-screen bg-light-background dark:bg-dark-background text-dark-primary dark:text-white"
   >
-    <router-view class="flex-grow" />
+    <transition name="load" type="out-in">
+      <router-view class="h-full w-full pb-10" />
+    </transition>
     <router-link
       to="/about"
-      class="text-light-accent dark:text-dark-accent text-center pb-8"
+      class="text-dark-primary dark:text-dark-accent text-center pb-8 absolute bottom-0 w-full"
     >
       About
     </router-link>
   </div>
 </template>
+
+<style>
+.load-enter-active,
+.load-leave-active {
+  position: absolute;
+  transition-property: all;
+  transition-duration: 100ms;
+}
+.load-enter {
+  opacity: 0;
+}
+.load-enter-active {
+  transition-delay: 100ms;
+}
+.load-leave-active {
+  opacity: 0;
+}
+</style>
