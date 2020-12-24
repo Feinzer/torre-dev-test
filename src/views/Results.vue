e46265475ea2b228931caf280508cca9910f42fd
Feinzer <danhernan23i@gmail.com>
2020-12-24 08:33:48 -0400

    Fix small padding

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -106,7 +106,7 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div class="flex flex-col w-full h-full items-center pb-12 overflow-y-auto">
+  <div class="flex flex-col w-full h-full items-center overflow-y-auto">
     <p v-if="!loading && searchResults.length > 0" class="my-16 text-2xl">
       Your results!
     </p>
@@ -181,7 +181,6 @@ export default Vue.extend({
                     d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                   ></path>
                 </svg>
-                <p class="pt-0.5 pl-2 md:hidden">Remote</p>
               </div>
             </div>
             <p class="mb-3">{{ mappedJobType(result) }}</p>
